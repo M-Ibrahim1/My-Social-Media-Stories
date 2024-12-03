a1bc4b15760eedcbb7ba9ee64ec41edf5913a0b7
M-Ibrahim1 <m.ibrahim@devsinc.com>
2024-12-03 19:40:17 +0500

    Added user profile picture management using active storage and resolved the corresponding merge conflicts

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,13 +3,17 @@ class ProfilesController < ApplicationController
 
   # Defining the action for "GET /profile"
   def show
-    render json: current_user.slice(:email, :name, :bio, :profile_picture, :gender), status: :ok
+    user_data = current_user.slice(:id, :email, :name, :bio, :gender)
+    user_data[:profile_picture_url] = rails_blob_path(current_user.profile_picture, only_path: true) if current_user.profile_picture.attached?
+    render json: user_data, status: :ok
   end
 
   # Defining the action for "PUT /profile"
   def update
     if current_user.update(profile_params)
-      render json: { user: current_user }, status: :ok
+      user_data = current_user.slice(:id, :email, :name, :bio, :gender)
+      user_data[:profile_picture_url] = rails_blob_path(current_user.profile_picture, only_path: true) if current_user.profile_picture.attached?
+      render json: { user: user_data }, status: :ok
     else
       render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
     end
